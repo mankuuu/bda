a1985bed99ed1f160705997f96e65f0b961746f4
Mayank Raj <65666477+mankuuu@users.noreply.github.com>
2024-07-15 03:05:03 +0530

    Update 3.java

--- 3.java
+++ 3.java
@@ -1,3 +1,10 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class maxtemperature {
 public static void main(String[] args) throws Exception {
 Configuration conf = new Configuration();
@@ -19,6 +26,12 @@ return;
 }
 
 //Mapper
+
+import java.io.IOException;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Mapper;
 public class MaxTempMapper extends Mapper<LongWritable, Text, Text,
 IntWritable > {
 public void map(LongWritable key, Text value, Context context) {
@@ -42,6 +55,11 @@ context.write(new Text(year),new IntWritable(airtemp));
 }
 
 //Reducer
+
+import java.io.IOException;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Reducer;
 public class MaxTempReducer extends Reducer<Text, IntWritable, Text,
 IntWritable>
 {
